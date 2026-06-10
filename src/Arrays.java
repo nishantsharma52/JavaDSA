254a33bbd64ca5635d9fa29642a3272f89c1e74e
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-10 13:23:04 +0530

    updated code

---
+++ src/Arrays.java
@@ -0,0 +1,157 @@
+import java.util.Scanner;
+
+public class Arrays {
+    static void main (){
+
+//       int arr[][] = {{1,2,3,},{1,2,3}} ;
+//       int sum = 0;
+//       for(int i=0; i<=arr.length-1; i++){
+//           for(int j=0; j<=arr[i].length-1; j++){
+//               int val = arr[i][j];
+//              sum = sum + val;
+//           }
+//
+//       }
+//        System.out.println(sum);
+
+
+
+
+
+//        int arr[][] = new int[4][5];
+//        Scanner sc = new Scanner(System.in);
+//        for(int i=0; i<=arr.length-1; i++){
+//            for(int j=0; j<=arr[i].length-1; j++){
+//                System.out.println("provide value for row=" + i + "and col " + j);
+//                arr[i][j] = sc.nextInt();
+//            }
+//
+//        }
+//
+//        for(int i = 0; i<=arr.length-1; i++){
+//            int colLength = arr[i].length;
+//            for(int j = 0; j<=colLength-1; j++){
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+//
+//        int[][] brr = {
+//                {1,2},{2,3,5,6,7},{3,4,7,6,5,4,3,2},{4},
+//        };
+//
+//        int rowLength = brr.length;
+////        int colLength = brr[0].length;
+//
+//        for(int i = 0; i<=rowLength-1; i++){
+//            int colLength = brr[i].length;
+//            for(int j = 0; j<=colLength-1; j++){
+//                System.out.print(brr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+//
+//        //decalartion
+//        int[][] arr;
+//        //allocation
+//        arr = new int[5][7];
+        //init
+//        int[][] brr = {
+//                {1,2},{2,3},{3,4},{4,5},
+//        };
+//
+//        int rowLength = brr.length;
+//        int colLength = brr[0].length;
+//
+//        for(int i = 0; i<=rowLength-1; i++){
+//            for(int j = 0; j<=colLength-1; j++){
+//                System.out.print(brr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+//
+//        int arr[] = { 1, 44, -55, 7 ,20};
+//        int n =  arr.length;
+//        int minVal = arr[0];
+//        for(int i = 0; i<=n-1; i++){
+//            if(arr[i]<minVal){
+//                minVal = arr[i];
+//            }
+//        }
+//        System.out.println(minVal);
+
+//        int arr[] = { 1, 44, 55, 7 ,20};
+//        int n =  arr.length;
+//        int maxVal = arr[0];
+//        for(int i = 0; i<=n-1; i++){
+//            if(arr[i]>maxVal){
+//                maxVal = arr[i];
+//            }
+//        }
+//        System.out.println(maxVal);
+
+//
+//        int arr[] = {1,2,3,4,5};
+//        int mul = 1;
+//        int n =  arr.length;
+//        for(int i = 0 ; i<=n-1; i++){
+//            mul = mul * arr[i];
+//        }
+//        System.out.println(mul);
+
+//        int arr[] = {10,20,30,40,50};
+//        int sum = 0;
+//        int n =  arr.length;
+//        for(int i = 0 ; i<=n-1; i++){
+//            sum = sum + arr[i];
+//        }
+//        System.out.println(sum);
+//
+//        int arr[] = { 11, 22, 33};
+//        int n = arr.length;
+//        for(int i = 0; i<=n-1; i++){
+//            System.out.println(arr[i]);
+//        }
+//
+
+
+//        int arr[] = new int[5];
+//        int n = arr.length;
+//        Scanner sc = new Scanner(System.in);
+//        //input
+//        for(int i = 0; i<=n-1; i++){
+//            System.out.println("provide value for index " +  i);
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        //print
+//        for(int val: arr){
+//            System.out.println( val);
+//        }
+
+
+//        //decalartion
+//        int arr[];
+//        //allocation
+//        arr = new int[5];
+//        //initilization
+//        int brr[] = {1, 2, 3};
+//
+////        System.out.println("value at 0 index " +  brr[0]);
+////        System.out.println("value at 1 index " +  brr[1]);
+////        System.out.println("value at 2 index " +  brr[2]);
+////         int n = brr.length;
+////         for(int index = 0; index<=n-1; index++){
+////             System.out.println(brr[index]);
+////         }
+//    // for each loop
+//        for(int var: brr){
+//            System.out.println(var);
+//        }
+
+    }
+}
