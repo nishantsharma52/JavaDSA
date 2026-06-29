9c6c5d864d0e781a06a42a7b95d72da6bc9a0556
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-29 16:19:34 +0530

    updated code

--- src/ArraysProblems/lac_27.java
+++ src/ArraysProblems/lac_27.java
@@ -79,8 +79,19 @@ public class lac_27 {
 //            return ans;
 //        }
 
+        static int getUnsortedElement(int[] arr){
+          for(int i=0; i<arr.length; i++){
+                if(arr[i+1]<=arr[i]){
+                    return arr[i+1];
+                }
+    }
+          return -1;
+        }
+
 
     static  void main(){
+    int[] arr = { 1,2, 1,5,4,8,9};
+        System.out.println(getUnsortedElement(arr));
 //
 //    int arr [] = { 0,1,0,0,1,1,0,0,0,1,1,};
 //    int ans[] = getZeroOneCount(arr);
