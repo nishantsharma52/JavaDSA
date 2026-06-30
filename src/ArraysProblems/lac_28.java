7a25637e6d18c1cb190a7b5a52c2548e28bac0c2
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-30 16:28:04 +0530

    updated code

---
+++ src/ArraysProblems/lac_28.java
@@ -0,0 +1,52 @@
+package ArraysProblems;
+
+public class lac_28 {
+//     problem 1   reverse an array....
+//    static  void reverseArray(int [] arr){
+//        int n =  arr.length;
+//        int i = 0;
+//
+//        int j = n -1;
+//        while (i<=j){
+//            //swap
+//            int temp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = temp;
+////            i ko aage bhadao
+//            i++;
+////            j ko aage badao
+//            j--;
+//        }
+//        for(int k : arr){
+//            System.out.println(k);
+//        }
+//
+        //problem 2 shift by 1
+
+//        static void shiftBy1(int[] arr){
+//            int n = arr.length;
+//            int temp = arr[n-1];
+//            for(int i = n-1; i>0; i--){
+//                arr[i] = arr[i-1];
+//        }
+//            arr[0] = temp;
+
+//    }
+
+
+
+
+
+
+
+    static void main() {
+//        int [] arr = {1,2,3,4,5};
+////        shiftBy1(arr);
+//        printAlternaate(arr);
+
+//        for(int a: arr){
+//            System.out.print(a + " ");
+//        }
+//        reverseArray(arr);
+    }
+}
