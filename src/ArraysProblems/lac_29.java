fa53a11610f030b64459177dbacb65798ac34cfc
Nishant Sharma <nishantsharma5330@gmail.com>
2026-07-02 19:09:48 +0530

    updated code

--- src/ArraysProblems/lac_29.java
+++ src/ArraysProblems/lac_29.java
@@ -6,31 +6,48 @@ public class lac_29 {
 
     // problem 1
 
-    static int[] sortArray(int[] arr){
+//    static int[] sortArray(int[] arr){
+//        int n = arr.length;
+//        int i=0;
+//        int j=n-1;
+//        while(i<=j){
+//            if(arr[i] == 1 && arr[j] == 0){
+//                arr[i] = 0;
+//                arr[j] = 1;
+//
+//            }
+//            if(arr[i] == 0){
+//                i++;
+//            }
+//            if(arr[j] == 1){
+//            j--;
+//           }
+//            }
+//        return arr;
+//    }
+    // problem 2
+    static int missingNumber(int[] arr){
+        int xorSum = 0;
+        for (int n : arr) {
+            xorSum = xorSum ^ n;
+            System.out.print(n + " " );
+            System.out.println(xorSum + " ");
+        }
         int n = arr.length;
-        int i=0;
-        int j=n-1;
-        while(i<=j){
-            if(arr[i] == 1 && arr[j] == 0){
-                arr[i] = 0;
-                arr[j] = 1;
-
-            }
-            if(arr[i] == 0){
-                i++;
-            }
-            if(arr[j] == 1){
-            j--;
-           }
-            }
-        return arr;
+        for (int i = 0; i <= n; i++) {
+            xorSum = xorSum ^ i;
+        }
+        return xorSum;
     }
 
 
+
     static void main() {
-        int arr[] = {1, 0, 1, 0, 0, 1, 0};
-        int ans[] = sortArray(arr);
-        System.out.println(Arrays.toString(ans));
+//        int arr[] = {1, 0, 1, 0, 0, 1, 0};
+//        int ans[] = sortArray(arr);
+//        System.out.println(Arrays.toString(ans));
+        int arr[] = {0, 1, 2, 3, 7, 5, 6};
+        System.out.println(missingNumber(arr));
 
     }
 }
