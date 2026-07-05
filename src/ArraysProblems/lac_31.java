98354fbcfc7565dea685259d43ab1f23646c42c4
Nishant Sharma <nishantsharma5330@gmail.com>
2026-07-05 10:23:18 +0530

    updated code

---
+++ src/ArraysProblems/lac_31.java
@@ -0,0 +1,33 @@
+package ArraysProblems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lac_31 {
+
+    static List<Integer> findDisappearedNumbers(int[] arr){
+        List<Integer> ans = new ArrayList<>();
+        int n = arr.length;
+        //Marking
+        for(int i = 0; i<n; i++){
+            int value = Math.abs(arr[i]);
+            int position = value-1;
+            //mark kr do agr positive h to
+            if(arr[position]>0){
+                arr[position] = - arr[position];
+            }
+        }
+        for(int i = 0; i<n; i++){
+            if(arr[i]>0){
+                ans.add(i+1);
+            }
+        }
+        return  ans;
+
+    }
+    static void main() {
+        int[] arr = {1,4,4,5,2,2};
+        System.out.println(findDisappearedNumbers(arr));
+
+    }
+}
