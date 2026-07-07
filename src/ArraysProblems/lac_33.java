467f4b034eaacd4e2a5124e1c922aa39c13d380a
Nishant Sharma <nishantsharma5330@gmail.com>
2026-07-07 07:59:14 +0530

    updated code

--- src/ArraysProblems/lac_33.java
+++ src/ArraysProblems/lac_33.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class lac_33 {
-    static List<Integer> rowSum(int[][] arr){
-        List<Integer> result =  new ArrayList<>();
-        int m = arr.length;
-        int n = arr[0].length;
-        for(int row = 0; row<m; row++){
-            //jese hi me kisi nyi row pr aauga me sum ki value ko zero kr dunga
-            int sum = 0;
-            for(int col=0; col<n; col++){
-                int value = arr[row][col];
-                sum = sum + value;
-            }
-            result.add(sum);
-        }
-        return  result;
-    }
+//    static List<Integer> rowSum(int[][] arr){
+//        List<Integer> result =  new ArrayList<>();
+//        int m = arr.length;
+//        int n = arr[0].length;
+//        for(int row = 0; row<m; row++){
+//            //jese hi me kisi nyi row pr aauga me sum ki value ko zero kr dunga
+//            int sum = 0;
+//            for(int col=0; col<n; col++){
+//                int value = arr[row][col];
+//                sum = sum + value;
+//            }
+//            result.add(sum);
+//        }
+//        return  result;
+//    }
 
     static List<Integer> colSum(int[][] arr){
         List<Integer> result = new ArrayList<>();
