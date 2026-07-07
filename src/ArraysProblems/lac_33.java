3a6112cb91c1be1654a52061f9f3064c3223f083
Nishant Sharma <nishantsharma5330@gmail.com>
2026-07-07 07:58:35 +0530

    updated code

--- src/ArraysProblems/lac_33.java
+++ src/ArraysProblems/lac_33.java
@@ -19,10 +19,26 @@ public class lac_33 {
         }
         return  result;
     }
+
+    static List<Integer> colSum(int[][] arr){
+        List<Integer> result = new ArrayList<>();
+        int n = arr.length;
+        int m = arr[0].length;
+        for(int col = 0; col<m; col++){
+            // jese hii nye col pr aauga sum ki vallue zero kr dunga
+            int sum = 0;
+            for(int row = 0; row<n; row++){
+                int val = arr[row][col];
+                sum = sum + val;
+            }
+            result.add(sum);
+        }
+        return result;
+    }
     static void main() {
         int[][] arr = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
 //        List<Integer> ans = rowSum(arr);
-        System.out.println(rowSum(arr));
+        System.out.println(colSum(arr));
 
     }
 
