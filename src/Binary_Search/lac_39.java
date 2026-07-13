46e3c22312149692e8800f9fa927d048bbf75e69
Nishant Sharma <nishantsharma5330@gmail.com>
2026-07-13 19:24:00 +0530

    updated code

--- src/Binary_Search/lac_39.java
+++ src/Binary_Search/lac_39.java
@@ -1,17 +1,17 @@
 package Binary_Search;
 
 public class lac_39 {
-    static int findPivotIndex(int[] arr){
-        int n = arr.length;
+    static int findPivotIndex(int[] nums){
+        int n = nums.length;
         int s = 0;
         int e = n-1;
         int ans = -1;
-        if(arr[s]<arr[e]){
+        if(nums[s]<nums[e]){
             return -1;
         }
         while(s<=e){
             int mid = s +(e-s)/2;
-            if(arr[mid]<=arr[n-1]){
+            if(nums[mid]<=nums[n-1]){
                 e = mid -1;
             }
             else{
@@ -21,9 +21,56 @@ public class lac_39 {
         }
         return ans;
     }
-    static void main(String[] args) {
-        int[] arr = { 50,60,70,10,20,30,40};
-        System.out.println(findPivotIndex(arr));
+    static int BinarySearch(int[] nums, int s, int e, int target){
+        int n = nums.length;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+
+            if(nums[mid] == target){
+                return  mid;
+            } else if (target>nums[mid]) {
+                s = mid + 1;
+
+            }
+            else{
+                e = mid-1;
+            }
+        }
+        return  -1;
+    }
+    public int search(int[] nums, int target){
+//        int[] nums  = {1,2,3,4,};
+        int n = nums.length;
+      int pivotIndex = findPivotIndex(nums);
+
+      // if pivotIndex = -1 then arrayy is already sorted
+//        int target = 110;
+       if(pivotIndex == -1){
+           int ans  = BinarySearch(nums , 0 ,n-1, target);
+          return ans;
+       }
+       else{
+           // array is not roated sorted
+           // array can be divided into l1 l2
+
+           //index of l1 bala part
+           int startArray1 = 0;
+           int endArray1 = pivotIndex;
+           if(target>=nums[startArray1] && target<=nums[endArray1]){
+               int ans = BinarySearch(nums,startArray1,endArray1,target);
+                return ans;
+           }
+
+           // index of l2 bala part
+           int startArray2 = pivotIndex + 1;
+           int endArray2 = n-1;
+           if(target>=nums[startArray2] && target<=nums[endArray2]){
+               int ans = BinarySearch(nums, startArray2 ,endArray2, target);
+               return ans;
+           }
+
+       }
+       return -1;
 
     }
 }
