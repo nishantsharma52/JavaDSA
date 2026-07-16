1ecdec760f5b54589bc855351af58d3cd801e7fc
Nishant Sharma <nishantsharma5330@gmail.com>
2026-07-16 12:20:55 +0530

    updated code

--- src/Binary_Search/lac_40.java
+++ src/Binary_Search/lac_40.java
@@ -4,7 +4,7 @@ public class lac_40 {
  static int mySqrt(int x) {
         int s = 1;
         int e = x;
-        int ans = -1;
+       double ans = -1;
         if(x==0){
             return 0;
         }
@@ -22,12 +22,34 @@ public class lac_40 {
                 e = mid -1;
             }
         }
-        return ans;
+     double factor = 1;
+     int totalPrecision = 3;
+     for (int round = 1; round <= totalPrecision; round++) {
+         factor = factor / 10;
+         for (int i = 1; i <= 10 ; i++) {
+             double newAns = ans + factor;
+             //equality code is ignored as of now, but make sure that you keep this in ur final code
+//                if (newAns * newAns == x) {
+//                    return newAns;
+//                }
+             if(newAns * newAns < x){
+                 ans = newAns;
+             }
+             else{
+                 break;
+             }
+         }
+         System.out.println("is round ka ans is: " + Math.round(ans * 1000.0) / 1000.0);
+     }
+     System.out.println("final round ka ans is: " + Math.round(ans * 1000.0) / 1000.0);
+
+     //returning dummy value
+     return -1;
 
     }
 
     static void main(String[] args) {
-     int x = 64;
+     int x = 56;
         System.out.println(mySqrt(x));
 
     }
