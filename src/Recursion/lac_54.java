53cecc4562c13c7af48b5eb54f5d8bbeac244099
Nishant Sharma <nishantsharma5330@gmail.com>
2026-09-05 17:19:51 +0530

    Recursion basic

--- src/Recursion/lac_54.java
+++ src/Recursion/lac_54.java
@@ -1,15 +1,35 @@
 package Recursion;
 
 public class lac_54 {
-    // factorial
+    // factorial using recursion
     static long factorial(int n){
         if(n == 0){
             return 1;
         }
         return n * factorial(n-1);
     }
+    // power of 2 using recursion
+    static int powerOfwo(int n){
+        if(n == 0){
+            return 1;
+        }
+        return 2 * powerOfwo(n-1);
+    }
+    //fibonacci number
+    static int fibonacci(int n){
+        if(n == 0){
+            return  0;
+        }
+        if(n == 1){
+            return  1;
+        }
+       int ans =  fibonacci(n-1) + fibonacci(n-2);
+        return ans;
+    }
     static void main(String[] args) {
         int n = 5;
         System.out.println(factorial(n));
+        System.out.println(powerOfwo(5));
+        System.out.println(fibonacci(4));
     }
 }
